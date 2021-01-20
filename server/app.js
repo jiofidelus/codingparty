d67510d67c5156202cf9609c1b053144017373a1
atemengue <moafembe@gmail.com>
2021-01-20 03:52:05 +0100

    add middleware

--- server/app.js
+++ server/app.js
@@ -2,9 +2,15 @@
 
 const express = require('express');
 const app = express();
+const logger = require('morgan');
+const cookiePaser = require('cookie-parser');
 
 const port = 3003;
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
 app.get('/', (req, res) => {
   res.send('Tech conference API');
 });
